64a87b5a53fce7b0d5badbdf9bfe8044cc7df397
Shamimkhan1406 <shamim4khan0@gmail.com>
2025-08-04 21:44:54 +0530

    sign in vendor

--- lib/controllers/vendor_auth_controller.dart
+++ lib/controllers/vendor_auth_controller.dart
@@ -1,14 +1,17 @@
+import 'dart:convert';
+
 import 'package:cartify_vendor/global_variables.dart';
 import 'package:cartify_vendor/models/vendor.dart';
 import 'package:cartify_vendor/services/manage_http_response.dart';
 import 'package:http/http.dart' as http;
 
 class VendorAuthController {
+  //signup vendor
   Future<void> signupVendor({required String fullName, required String email, required String password, required context }) async {
 
     try {
       Vendor vendor = Vendor(id: '', fullName: fullName, email: email, state: '', city: '', locality: '', role: '', password: password);
-      http.Response response = await http.post(Uri.parse("$uri/api/vendor/signin"),
+      http.Response response = await http.post(Uri.parse("$uri/api/vendor/signup"),
       body: vendor.toJson(),
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
@@ -16,6 +19,25 @@ class VendorAuthController {
       );
       manageHttpResponse(response: response, context: context, onSuccess: (){
         showSnackBar(context, "Vendor signed up successfully");
+      });
+    } catch (e) {
+      showSnackBar(context, e.toString());
+    }
+  }
+  // signin vendor
+  Future<void> signinVendor({required String email, required String password, required context}) async{
+    try {
+      http.Response response = await http.post(Uri.parse("$uri/api/vendor/signin"),
+      body: jsonEncode({
+        'email': email,
+        'password': password,
+      }),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      );
+      manageHttpResponse(response: response, context: context, onSuccess: (){
+        showSnackBar(context, "Vendor signed in successfully");
       });
     } catch (e) {
       showSnackBar(context, e.toString());
