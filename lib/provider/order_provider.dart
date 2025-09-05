fae3334630947b1bf4caf61eba0a1bfb4561bcd6
Shamimkhan1406 <shamim4khan0@gmail.com>
2025-09-05 19:52:10 +0530

    recreating order state for realtime update

--- lib/provider/order_provider.dart
+++ lib/provider/order_provider.dart
@@ -1,13 +1,40 @@
-
 import 'package:cartify_vendor/models/order.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class OrderProvider extends StateNotifier<List<Order>> {
   OrderProvider() : super([]);
   // set the list of orders
-  void setOrders(List<Order> orders){
+  void setOrders(List<Order> orders) {
     state = orders;
+  }
+
+  void updateOrderStatus(String orderId, {bool? delivered, bool? processing}) {
+    state = [
+      for (final order in state)
+        if (order.id == orderId)
+          Order(
+            id: order.id,
+            fullName: order.fullName,
+            email: order.email,
+            state: order.state,
+            city: order.city,
+            locality: order.locality,
+            productName: order.productName,
+            productPrice: order.productPrice,
+            quantity: order.quantity,
+            category: order.category,
+            image: order.image,
+            buyerId: order.buyerId,
+            vendorId: order.vendorId,
+            processing: processing ?? order.processing,
+            delivered: delivered ?? order.delivered,
+          )
+        else
+          order,
+    ];
   }
 }
 
-final orderProvider = StateNotifierProvider<OrderProvider, List<Order>>((ref) => OrderProvider());
+final orderProvider = StateNotifierProvider<OrderProvider, List<Order>>(
+  (ref) => OrderProvider(),
+);
