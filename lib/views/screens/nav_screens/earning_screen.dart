0a3c2b238c3cd47ac1d4ce3b0b59d1cb58441fac
Shamimkhan1406 <shamim4khan0@gmail.com>
2025-09-06 12:26:02 +0530

    display vendors total earnings

--- lib/views/screens/nav_screens/earning_screen.dart
+++ lib/views/screens/nav_screens/earning_screen.dart
@@ -1,3 +1,6 @@
+import 'package:cartify_vendor/controllers/order_controller.dart';
+import 'package:cartify_vendor/provider/order_provider.dart';
+import 'package:cartify_vendor/provider/total_earning_provider.dart';
 import 'package:cartify_vendor/provider/vendor_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -11,9 +14,34 @@ class EarningScreen extends ConsumerStatefulWidget {
 }
 
 class _EarningScreenState extends ConsumerState<EarningScreen> {
+  void initState() {
+    super.initState();
+    // fetch orders when the screen is initialized
+    fetchOrders();
+  }
+
+  Future<void> fetchOrders() async {
+    final user = ref.read(vendorProvider);
+    if (user != null) {
+      // fetch orders using the user id
+      // final orders = await OrderController().fetchOrders(user.id);
+      // update the order provider with the fetched orders
+      // ref.read(orderProvider.notifier).setOrders(orders);
+      final OrderController orderController = OrderController();
+      try {
+        final orders = await orderController.loadOrders(vendorId: user.id);
+        ref.read(orderProvider.notifier).setOrders(orders);
+        ref.read(totalEarningsProvider.notifier).calculateEarnings(orders);
+      } catch (e) {
+        print('Error fetching orders: $e');
+      }
+    }
+  }
+  
   @override
   Widget build(BuildContext context) {
     final vendor = ref.watch(vendorProvider);
+    final totalEarnings = ref.watch(totalEarningsProvider);
     return Scaffold(
       appBar: AppBar(
         title: Row(
@@ -39,7 +67,24 @@ class _EarningScreenState extends ConsumerState<EarningScreen> {
           ],
         ),
       ),
-      body: const Center(child: Text("Earning Screen")),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Center(child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Total Earnings", style: GoogleFonts.montserrat(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),),
+            SizedBox(height: 8),
+            Text("₹${totalEarnings.toStringAsFixed(2)}", style: GoogleFonts.montserrat(
+              fontSize: 36,
+              fontWeight: FontWeight.bold,
+              color: Colors.green,
+            ),),
+          ],
+        )),
+      ),
     );
   }
 }
