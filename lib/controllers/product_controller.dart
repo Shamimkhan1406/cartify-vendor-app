dcd5228f7dcb3cbc810d39c8b140d2cd4aae4c90
Shamimkhan1406 <shamim4khan0@gmail.com>
2025-10-18 17:58:31 +0530

    implementing the product edit api

--- lib/controllers/product_controller.dart
+++ lib/controllers/product_controller.dart
@@ -5,6 +5,7 @@ import 'package:cartify_vendor/global_variables.dart';
 import 'package:cartify_vendor/models/product.dart';
 import 'package:cartify_vendor/services/manage_http_response.dart';
 import 'package:cloudinary_public/cloudinary_public.dart';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -57,12 +58,15 @@ class ProductController {
               'x-auth-token': token!,
             },
           );
-          manageHttpResponse(response: response, context: context, onSuccess: (){
-            showSnackBar(context, "Product added successfully");
-            //Navigator.pop(context);
-          });
-        }
-        else{
+          manageHttpResponse(
+            response: response,
+            context: context,
+            onSuccess: () {
+              showSnackBar(context, "Product added successfully");
+              //Navigator.pop(context);
+            },
+          );
+        } else {
           showSnackBar(context, 'Please select a category and subcategory');
         }
       }
@@ -76,11 +80,12 @@ class ProductController {
   }
 
   // display related product by subcategory
-  Future<List<Product>> loadVendorsProduct(String vendorId)async {
+  Future<List<Product>> loadVendorsProduct(String vendorId) async {
     try {
       SharedPreferences prefs = await SharedPreferences.getInstance();
       String? token = prefs.getString('auth_token');
-      http.Response response = await http.get(Uri.parse('$uri/api/products/$vendorId'),
+      http.Response response = await http.get(
+        Uri.parse('$uri/api/products/$vendorId'),
         headers: <String, String>{
           'Content-Type': 'application/json; charset=UTF-8',
           'x-auth-token': token!,
@@ -92,21 +97,61 @@ class ProductController {
         final List<dynamic> data = jsonDecode(response.body) as List<dynamic>;
 
         // map the list of dynamic objects to a list of Product objects
-        List<Product> vendorsProduct = data.map((product)=> Product.fromMap(product as Map<String, dynamic>)).toList();
+        List<Product> vendorsProduct =
+            data
+                .map(
+                  (product) => Product.fromMap(product as Map<String, dynamic>),
+                )
+                .toList();
         return vendorsProduct;
-      } 
-      else if(response.statusCode == 404){
+      } else if (response.statusCode == 404) {
         return [];
-      }
-      else {
+      } else {
         // if the response status code is not 200, throw an error
         throw Exception('Failed to load vendors Products');
       }
     } catch (e) {
       // Handle error
       throw Exception('Error loading vemdors Products: $e');
-      
     }
   }
 
+  Future<void> updateProduct({
+    required Product product,
+    required BuildContext context,
+    required List<File>? pickedImages,
+  }) async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    String? token = preferences.getString('auth_token');
+    List<String> images = product.images;
+    if (pickedImages != null) {
+      final cloudinary = CloudinaryPublic(cloudName, uploadPreset);
+      List<String> images = [];
+      // loop through each image in the pickedImages list
+      for (File pickedImage in pickedImages) {
+        CloudinaryResponse cloudinaryResponse = await cloudinary.uploadFile(
+          CloudinaryFile.fromFile(
+            pickedImage.path,
+            folder: product.productName,
+          ),
+        );
+        // add the secure URL of the uploaded image to the images list
+        images.add(cloudinaryResponse.secureUrl);
+      }
+    }
+    final updatedData = product.toMap();
+    updatedData['images'] = images;
+
+    http.Response response = await http.put(
+      Uri.parse('$uri/api/edit-product/${product.id}'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+        'x-auth-token': token!,
+      },
+      body: jsonEncode(updatedData),
+    );
+    manageHttpResponse(response: response, context: context, onSuccess: (){
+      showSnackBar(context, 'Product updated successfully');
+    });
+  }
 }
