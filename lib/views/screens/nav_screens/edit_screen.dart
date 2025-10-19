075ee7bf18641133511dca0a9d0cbea76d6c6c6e
Shamimkhan1406 <shamim4khan0@gmail.com>
2025-10-19 15:49:26 +0530

    custom edit product appbar

--- lib/views/screens/nav_screens/edit_screen.dart
+++ lib/views/screens/nav_screens/edit_screen.dart
@@ -4,6 +4,7 @@ import 'package:cartify_vendor/provider/vendor_provider.dart';
 import 'package:cartify_vendor/views/screens/detail/screens/edit_product_detail_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class EditScreen extends ConsumerStatefulWidget {
   const EditScreen({super.key});
@@ -51,24 +52,90 @@ class _EditScreenState extends ConsumerState<EditScreen> {
     if (products.isEmpty) {
       return const Center(child: CircularProgressIndicator());
     }
-    return isLoading
-        ? const Center(child: CircularProgressIndicator())
-        : SizedBox(
-          height: 250,
-          child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-            itemCount: products.length,
-            itemBuilder: (context, index) {
-              final product = products[index];
-              return InkWell(
-                onTap: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => EditProductDetailScreen(product: product)));
-                },
-                child: Center(child: Text(product.productName)),
-              );
-              //ProductItemWidget(product: product);
-            },
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(
+          MediaQuery.of(context).size.height * 0.2,
+        ),
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: 118,
+          clipBehavior: Clip.hardEdge,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/icons/cartb.png"),
+              fit: BoxFit.cover,
+            ),
           ),
-        );
+          child: Stack(
+            children: [
+              Positioned(
+                left: 322,
+                top: 52,
+                child: Stack(
+                  children: [
+                    Image.asset("assets/icons/not.png", width: 25, height: 25),
+                    Positioned(
+                      top: 0,
+                      right: 0,
+
+                      child: Container(
+                        height: 20,
+                        width: 20,
+                        padding: const EdgeInsets.all(4),
+                        decoration: BoxDecoration(
+                          color: const Color.fromARGB(255, 255, 250, 201),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Center(
+                          child: Text(
+                            products.length.toString(),
+                            style: TextStyle(
+                              fontSize: 12,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Positioned(
+                left: 61,
+                top: 51,
+                child: Text(
+                  'Edit Products',
+                  style: GoogleFonts.lato(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : SizedBox(
+            height: 250,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: products.length,
+              itemBuilder: (context, index) {
+                final product = products[index];
+                return InkWell(
+                  onTap: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => EditProductDetailScreen(product: product)));
+                  },
+                  child: Center(child: Text(product.productName)),
+                );
+                //ProductItemWidget(product: product);
+              },
+            ),
+          ),
+    );
   }
 }
